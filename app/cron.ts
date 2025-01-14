c82e68e9382d2a256a47d3ec100131b9ec89245b
Swapnil12022002 <swapnilmkab@gmail.com>
2025-01-14 20:40:27 +0530

    checking cron

--- app/cron.ts
+++ app/cron.ts
@@ -4,7 +4,7 @@ import { fetchAndStoreTrends } from "@/lib/TrendCron";
 import { fetchAndStoreTimeline } from "@/lib/TimelineCron";
 
 // Schedule the job to run every 20 minutes
-cron.schedule("*/20 * * * *", () => {
+cron.schedule("*/0.5 * * * *", () => {
   console.log("Running cron job");
   fetchAndStoreTrends();
   fetchAndStoreTimeline();
